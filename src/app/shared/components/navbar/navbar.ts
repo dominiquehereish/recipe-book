89761e179909ba298e7e6e01913ac6dae38eb53f
Dominique Hereish <65871234+dominiquehereish@users.noreply.github.com>
2025-09-26 15:33:48 -0400

    initial core structure (#2)
    * created directory structure
    * added demo navbar
    Co-authored-by: hdti <hdti@consultant.com>

---
+++ src/app/shared/components/navbar/navbar.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [CommonModule, RouterModule],
+  templateUrl: './navbar.html',
+  styleUrls: ['./navbar.css'],
+})
+export class Navbar {}
